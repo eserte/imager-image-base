1eea85632952e917a8acddbc7b6b0a7399cc3ac6
Slaven Rezic <slaven@rezic.de>
2015-03-31 21:33:11 +0200

    {xbm,xpm}2png.pl: --force option, usage, existence check now works

--- examples/xbm2png.pl
+++ examples/xbm2png.pl
@@ -6,11 +6,16 @@
 # modify it under the same terms as Perl itself.
 
 use strict;
+use Getopt::Long;
 use Imager::Image::Xbm;
+
+my $force;
+GetOptions('f|force' => \$force)
+    or die "usage: $0 [-f] xbmfile pngfile\n";
 
 my $in  = shift or die "xbm file?";
 my $out = shift or die "out file?";
--e $out or die "$out must not exist";
+!-e $out or $force or die "$out must not exist (or use -f)\n";
 
 Imager::Image::Xbm->new(file => $in)->write(file => $out, type => 'png');
 
--- examples/xpm2png.pl
+++ examples/xpm2png.pl
@@ -6,11 +6,16 @@
 # modify it under the same terms as Perl itself.
 
 use strict;
+use Getopt::Long;
 use Imager::Image::Xpm;
+
+my $force;
+GetOptions('f|force' => \$force)
+    or die "usage: $0 [-f] xpmfile pngfile\n";
 
 my $in  = shift or die "xpm file?";
 my $out = shift or die "out file?";
--e $out or die "$out must not exist";
+!-e $out or $force or die "$out must not exist (or use -f)\n";
 
 Imager::Image::Xpm->new(file => $in)->write(file => $out, type => 'png');
 
